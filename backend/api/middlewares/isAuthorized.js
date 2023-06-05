2ba1b0b4aef9acb5710d326c3a86c3270dc867a8
Bethelhemm <122419774+Bethelhemm@users.noreply.github.com>
2023-06-05 10:20:20 +0300

    Update isAuthorized.js
    updates

--- backend/api/middlewares/isAuthorized.js
+++ backend/api/middlewares/isAuthorized.js
@@ -1,1 +1,18 @@
+const Doctor = require('../models/doctor.model');
+//used to ensure that only authorized hospitals can perform CRUD operations on the doctor accounts.
+exports.isAuthorized = async (req, res, next) => {
+    try {
+        const doctorId = req.params.doctorId;
+        const hospitalId = req.params.hospitalId;
 
+        const doctor = await Doctor.findById(doctorId);
+
+        if (!doctor || doctor.hospital.toString() !== hospitalId) {
+            return res.status(401).json({ message: 'Unauthorized access' });
+        }
+
+        next();
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
