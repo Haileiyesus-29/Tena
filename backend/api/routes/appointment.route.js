627525b5109df256aa34b81d1300818471db9158
Bethelhemm <122419774+Bethelhemm@users.noreply.github.com>
2023-06-06 20:38:32 +0300

    Update appointment.route.js

--- backend/api/routes/appointment.route.js
+++ backend/api/routes/appointment.route.js
@@ -1,6 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const appointmentController = require('../controllers/appointment.controller');
+const { validateUserId, validateHospitalId, validateDoctorId, checkDoctorScheduleConflict } = require('../middlewares/appointmentsMiddlewares');
+const { checkUserAuth } = require('../middlewares/checkUserAuth');
 
-router.get('/')
+// Create a new appointment
+router.post('/create', validateUserId, validateHospitalId, validateDoctorId, checkDoctorScheduleConflict, appointmentController.createAppointment);
 
-module.exports = router
+// Update an existing appointment
+router.put('/update/:id', validateUserId, validateHospitalId, validateDoctorId, checkDoctorScheduleConflict, checkUserAuth, appointmentController.updateAppointment);
+
+// Delete an appointment
+router.delete('/delete/:id', appointmentController.deleteAppointment);
+
+// Get all appointments for a hospital
+router.get('/hospital/:id', appointmentController.getAppointmentsForHospital);
+
+module.exports = router;
